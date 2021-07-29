da292ffbdca8ab7bbc5935b7e54d2a9d4f6174c9
ankit <69717009+crew-guy@users.noreply.github.com>
2021-07-29 15:11:41 +0530

    tab screen added

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/screens/category_screen.dart';
+import 'package:food_app/screens/favourites_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({Key? key}) : super(key: key);
+
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals'),
+        bottom: TabBar(
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.category), text: 'Categories'),
+            Tab(icon: Icon(Icons.star), text: 'Favourites'),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        children: <Widget>[
+          CategoryScreen(),
+          FavouritesScreen(),
+        ],
+      ),
+    );
+  }
+}
